fc748a79ff674eb6c2dfa6b90c44f8f054810463
Muhammad Dhafa Ramadhani <dhafaramadhani2004@gmail.com>
2024-05-24 13:03:43 +0700

    menggunakan bloc listener

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,15 +9,33 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Belajar Cubit'),
-        ),
-        body: SafeArea(
-          child: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              BlocBuilder(
+      appBar: AppBar(
+        title: const Text('Belajar Cubit'),
+      ),
+      body: SafeArea(
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // bloc listener ini berfungsi untuk menampilkan sebuah fungsi
+            // yang berjalan bersama dengan blocBuildernya,
+            // biasanya bloc listener itu untuk membuat sebuah snackBar atau dialog
+            BlocListener<BlocService, int>(
+              bloc: blocService,
+              listenWhen: (previous, current) {
+                if (current % 2 == 0) {
+                  return true;
+                } else {
+                  return false;
+                }
+              },
+              listener: (context, state) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text("data ini genap ke $state"),
+                  duration: Duration(seconds: 1),
+                ));
+              },
+              child: BlocBuilder<BlocService, int>(
                 bloc: blocService,
                 builder: (context, state) {
                   return Text(
@@ -26,27 +44,28 @@ class Home extends StatelessWidget {
                   );
                 },
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        blocService.tambahData();
-                      },
-                      icon: const Icon(Icons.add)),
-                  IconButton(
-                      onPressed: () {
-                        blocService.kurangData();
-                      },
-                      icon: const Icon(Icons.remove)),
-                ],
-              )
-            ],
-          )),
-        ),
-      );
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      blocService.tambahData();
+                    },
+                    icon: const Icon(Icons.add)),
+                IconButton(
+                    onPressed: () {
+                      blocService.kurangData();
+                    },
+                    icon: const Icon(Icons.remove)),
+              ],
+            )
+          ],
+        )),
+      ),
+    );
   }
 }
