7ab70f22b79b25f7844d724b6c16fe88e42af100
Muhammad Dhafa Ramadhani <dhafaramadhani2004@gmail.com>
2024-05-25 05:34:02 +0700

    pengiriman data menggunakan provider.value dengan push route

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
+import 'package:belajar_flutter_github/bloc/bloc_service.dart';
+import 'package:belajar_flutter_github/pages/page_ke_2.dart';
 import 'package:belajar_flutter_github/widgets/data_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -10,8 +13,19 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Bloc Provider"),
       ),
-      body: const Center(
-        child: DataWidget()
+      body: const Center(child: DataWidget()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => BlocProvider.value(
+                    // value: context.read<BlocService>(), // bisa juga seperti ini
+                    value: BlocProvider.of<BlocService>(context), // bisa juga seperti ini
+                    child: const Page2()
+                    )));
+        },
+        child: const Icon(Icons.arrow_right),
       ),
     );
   }
---
+++ lib/pages/page_ke_2.dart
@@ -0,0 +1,33 @@
+import 'package:belajar_flutter_github/bloc/bloc_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.fork_left)),
+        centerTitle: true,
+        title: const Text("Ini page ke 2"),
+      ),
+      body: Center(
+        child: BlocBuilder(
+          bloc: BlocProvider.of<BlocService>(context),
+          builder: (context, state) {
+            return Text(
+            "$state",
+            style: const TextStyle(fontSize: 50, color: Colors.black),
+          );}
+        ),
+      ),
+    );
+  }
+}
